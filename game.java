e6e7914fea0269a258ac7edf7fb53569675b5b14
kanika1997 <34895969+kanika1997@users.noreply.github.com>
2018-01-02 01:14:58 +0530

    Add files via upload

--- game.java
+++ game.java
@@ -1,144 +1,57 @@
-package first;
+package kanika;
 
-
-import java.util.Scanner;
-
-public class game {
-public static void main(String[] args) {
-		
-		
-		// TODO Auto-generated method stub
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("enter queries");
-       int q=scanner.nextInt();
-       int i;
-       for(i=0;i<q;i++) {
-    	   System.out.println("enter array size");
-    	   int n = scanner.nextInt();
-    	   System.out.println("enter leap");
-    	   int leap = scanner.nextInt();
-    	   int j;
-    	   System.out.println("enter array");
-    	   int[] Array = new int[n];
-    	   for (j=0; j<n;j++) {
-    		   Array[j]=scanner.nextInt();    		   
-    	   }
-       
-    	   int position = -1;
-    	   int k=0;
-    	   
-    		   while(k<n){
-    			   if(position == n-1 || position>=n) {
-    				   break;
-    			   }	   
-           else if(position+1<=n && Array[position+1]==0)
-    	   {
-    		  position++;
-    		 
-    	  }  
-           else if ((position+leap)>n) {
-    		   position=n;
-    		   break;
-		   }
-		   else if (Array[(position+1)+leap]==0) {
-	    		  position=position + leap;
-	    		 
-		   }  
-    	   k++;
-    	   if ((position+leap)==1)
-    	   {
-    		   position--;
-    		   k--;
-    	   }
-    		}
-       	
-    	   if(position == n-1 || position>=n)
-    	   {
-    		   System.out.println("you won");
-    	   }
-    	   else {
-    		   System.out.println("you lose");
-    	   }
-       
-       }
+public class game{
+	private int n;
+	private int leap;
+	int arr[]= new int[n];
+	int position = -1;
+	int k=0;
+    int x =1;
+	  
+	public void MovesBackwards() {
+		if(position-1>=0 && arr[position-1]==0) {
+			position--;
+			if(position+leap<n && arr[position+leap] == 0) {
+				position = position+leap;
+			}
+		}
+		else {
+			System.out.println("You lose");
+		}
+	}
 	
-scanner.close();
-
-}
-/*
-	Scanner scanner = new Scanner(System.in);
-    int i,n,leap,x=0,y=0;
-    game(int n,int leap){
-    	this.n=n;
-    	this.leap = leap;
-    	creategame();
-    	
-    }
+	public void MovesForward() {
 		
-		public void creategame(){
-		// TODO Auto-generated method stub
-			System.out.println("enter queries");
-			 int q=scanner.nextInt();
-       for(i=0;i<q;i++) {
-    	   System.out.println("enter array size");
-    	   n = scanner.nextInt();
-    	   System.out.println("enter leap");
-    	   leap = scanner.nextInt();
-    	   int j;
-    	   System.out.println("enter array");
-    	   int[] Array = new int[n];
-    	   for (j=0; j<n;j++) {
-    		   Array[j]=scanner.nextInt();    		   
-    	   }
-       
-       public void Forward() {
-    	   int position = -1;
-    	   int k=0;
-    	   
-    		   while(k<n){
-    	   if(position+1<=n && Array[position+1]==0)
-    	   {
-    		  position++; 
-    	  }  
-    		   
-    	   else if ((position+leap)>n) {
-    		   position=n;
-    		   break;
-    		   
-    	   }
-    	   else if (Array[position+leap]==0) {
-    		  position=position + leap;
-    		   }
-    	   
-    	   k++;
-    		}
-       	
-    	   if(position == n-1 || position>=n)
-    	   {
-    		  x++;
-    	   }
-    	   else {
-    		   y++;
-    	   }
-       }
-       }
-     // while(x>-1 || y>-1) {
-      // if(x>0) {
-    	   while(x>0) {
-    		   System.out.println("yes");
-    		   x--;
-    	   }
-       //}
-        //if (y>0) {
-    	   while(y>0) {
-    		   System.out.println("no");
-    		   y--;
-    	//   }
-    //   }
-       }
-	
-scanner.close();
-
-}
-*/
+		
+		while(k<n){
+			if(position+1<=n && arr[position+1] == 0) {
+				position++;
+			    break;
+			}
+			else if(position+leap>n){
+				position = n;
+				break;
+			}
+			else if(position+leap<=n && arr[position+leap]==0) {
+				position = position+leap;
+				break;
+			}
+			
+			k++;
+		}		
+		 if(position == n-1 || position>=n)
+  	   {
+  		
+  		  System.out.println("you won");
+  	   }
+  	   else {
+  		   x=0;
+  		   System.out.println("you lose");
+  	   }
+		 if(x==0) {
+			 while(arr[position]==0) {
+				 MovesBackwards();
+			 }
+		 }
+	}
 }
